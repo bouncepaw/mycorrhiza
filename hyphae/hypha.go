bee862563d8f1f02ef121ea257768abae7917940
bouncepaw <bouncepaw2@ya.ru>
2021-01-04 02:10:33 +0500

    Auto-detect links

--- hyphae/hypha.go
+++ hyphae/hypha.go
@@ -1,8 +1,6 @@
 package hyphae
 
 import (
-	"errors"
-
 	"github.com/hashicorp/go-memdb"
 )
 
@@ -15,8 +13,23 @@ type Hypha struct {
 	BackLinks  []string
 }
 
-func AddHypha(h Hypha) error {
-	return errors.New("Not implemented")
+// AddHypha adds a hypha named `name` with such `textPath` and `binaryPath`. Both paths can be empty. Does //not// check for hypha's existence beforehand. Count is handled.
+func AddHypha(name, textPath, binaryPath string) {
+	txn := db.Txn(true)
+	txn.Insert("hyphae",
+		&Hypha{
+			Name:       name,
+			TextPath:   textPath,
+			BinaryPath: binaryPath,
+			OutLinks:   make([]string, 0),
+			BackLinks:  make([]string, 0),
+		})
+	txn.Commit()
+	IncrementCount()
+}
+
+// DeleteHypha clears both paths and all out-links from the named hypha and marks it as non-existent. It does not actually delete it from the memdb. Count is handled.
+func DeleteHypha(name string) {
 }
 
 // Create the DB schema
