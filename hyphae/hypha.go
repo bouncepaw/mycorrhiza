f34757afbd7a3c73b7496963fcfe2a0094e17d09
bouncepaw <bouncepaw2@ya.ru>
2021-01-24 13:36:03 +0500

    Drop memdb dependency because it is not used yet

--- hyphae/hypha.go
+++ hyphae/hypha.go
@@ -1,8 +1,7 @@
 package hyphae
 
-import (
-	"github.com/bouncepaw/mycorrhiza/storage"
-)
+// TODO: do
+import ()
 
 type Hypha struct {
 	Name       string
@@ -15,17 +14,6 @@ type Hypha struct {
 
 // AddHypha adds a hypha named `name` with such `textPath` and `binaryPath`. Both paths can be empty. Does //not// check for hypha's existence beforehand. Count is handled.
 func AddHypha(name, textPath, binaryPath string) {
-	txn := storage.DB.Txn(true)
-	txn.Insert("hyphae",
-		&Hypha{
-			Name:       name,
-			TextPath:   textPath,
-			BinaryPath: binaryPath,
-			OutLinks:   make([]string, 0),
-			BackLinks:  make([]string, 0),
-		})
-	txn.Commit()
-	IncrementCount()
 }
 
 // DeleteHypha clears both paths and all out-links from the named hypha and marks it as non-existent. It does not actually delete it from the memdb. Count is handled.
