24380f3775445c0c8ec65cca55daaded279a78f0
Timur Ismagilov <bouncepaw2@ya.ru>
2021-05-17 00:51:46 +0500

    Remove the background pattern, make the top bar lighter in the light theme

--- assets/assets.qtpl.go
+++ assets/assets.qtpl.go
@@ -357,12 +357,11 @@ table { background-color: #eee; }
 .layout-card__title { background-color: #eee; }
 
 /* Other stuff */
-html { background-color: #ddd; 
-background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='199' viewBox='0 0 100 199'%3E%3Cg fill='%23bbbbbb' %3E%3Cpath d='M0 199V0h1v1.99L100 199h-1.12L1 4.22V199H0zM100 2h-.12l-1-2H100v2z'%3E%3C/path%3E%3C/g%3E%3C/svg%3E");
-} /* heropatterns.com */
-header { background-color: #bbb; }
+html { background-color: #ddd;
+}
+header { background-color: #eee; }
 .header-links__link { color: black; }
-.header-links__link:hover { background-color: #eee; }
+.header-links__link:hover { background-color: #ddd; }
 main { background-color: white; }
 
 blockquote { border-left: 4px black solid; }
