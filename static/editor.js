9ad9db98258f91f69c2a9900f825d0cb0ba54da3
handlerug <umar@handlerug.me>
2021-07-13 21:21:46 +0700

    Fix warn before closing

--- static/editor.js
+++ static/editor.js
@@ -1,14 +1,21 @@
 (function () {
 	let changed = false;
 	let textarea = document.querySelector('.edit-form__textarea');
+	let form = document.querySelector('.edit-form');
 
 	let warnBeforeClosing = function (ev) {
+		if (!changed) return;
 		ev.preventDefault();
 		return ev.returnValue = 'Are you sure you want to exit? You have unsaved changes.';
 	};
 
 	textarea.addEventListener('input', function () {
-		if (!changed) window.addEventListener('beforeunload', warnBeforeClosing);
 		changed = true;
 	});
+
+	form.addEventListener('submit', function () {
+		changed = false;
+	});
+
+	window.addEventListener('beforeunload', warnBeforeClosing);
 })();
