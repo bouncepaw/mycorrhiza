92b065ba941a9bc658dd656fc2cf08b2d526d633
Mikhail Chekan <chekoopa@mail.ru>
2021-09-01 01:12:55 +0800

    Separate pathographic sorting code

--- shroom/search.go
+++ shroom/search.go
@@ -1,7 +1,6 @@
 package shroom
 
 import (
-	"sort"
 	"strings"
 
 	"github.com/bouncepaw/mycorrhiza/hyphae"
@@ -11,37 +10,16 @@ import (
 func YieldHyphaNamesContainingString(query string) <-chan string {
 	query = util.CanonicalName(query)
 	out := make(chan string)
+	sorted := hyphae.PathographicSort(out)
 	go func() {
-		// To make it unicode-friendly and lean, we cast every string into rune slices, sort, and only then cast them back
-		raw := make([][]rune, 0)
 		for h := range hyphae.YieldExistingHyphae() {
 			if hyphaNameMatchesString(h.Name, query) {
-				raw = append(raw, []rune(h.Name))
+				out <- h.Name
 			}
-		}
-		sort.Slice(raw, func(i, j int) bool {
-			const slash rune = 47 // == '/'
-			// Classic lexicographical sort with a twist
-			c := 0
-			for {
-				if c == len(raw[i]) { return true }
-				if c == len(raw[j]) { return false }
-				if raw[i][c] == raw[j][c] {
-					c++
-				} else {
-					// The twist: subhyphae-awareness is about pushing slash upwards
-					if raw[i][c] == slash { return true }
-					if raw[j][c] == slash { return false }
-					return raw[i][c] < raw[j][c]
-				}
-			}
-		})
-		for _, name := range raw {
-			out <- string(name)
 		}
 		close(out)
 	}()
-	return out
+	return sorted
 }
 
 // This thing gotta be changed one day, when a hero has time to implement a good searching algorithm.
