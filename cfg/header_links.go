80d558317950b204efa13454c1c1137d5598b0a5
Timur Ismagilov <bouncepaw2@ya.ru>
2021-12-19 17:35:18 +0300

    Mycomarkup: Migrate to v3.5.0
    Image layouts!

--- cfg/header_links.go
+++ cfg/header_links.go
@@ -2,8 +2,9 @@ package cfg
 
 // See https://mycorrhiza.wiki/hypha/configuration/header
 import (
+	"github.com/bouncepaw/mycomarkup/v3"
 	"github.com/bouncepaw/mycomarkup/v3/blocks"
-	"strings"
+	"github.com/bouncepaw/mycomarkup/v3/mycocontext"
 )
 
 // HeaderLinks is a list off current header links. Feel free to iterate it directly but do not modify it by yourself. Call ParseHeaderLinks if you need to set new header links.
@@ -21,24 +22,19 @@ func SetDefaultHeaderLinks() {
 
 // ParseHeaderLinks extracts all rocketlinks from the given text and saves them as header links.
 func ParseHeaderLinks(text string) {
-	HeaderLinks = []HeaderLink{}
-	for _, line := range strings.Split(text, "\n") {
-		// There is a false positive when parsing markup like that:
-		//
-		//     ```
-		//     => this is not a link, it is part of the preformatted block
-		//     ```
-		//
-		// I do not really care.
-		if strings.HasPrefix(line, "=>") {
-			rl := blocks.ParseRocketLink(line, HeaderLinksHypha)
-			href, display := rl.Href(), rl.Display()
-			HeaderLinks = append(HeaderLinks, HeaderLink{
-				Href:    href,
-				Display: display,
-			})
+	ctx, _ := mycocontext.ContextFromStringInput("", text)
+	// We call for side-effects
+	_ = mycomarkup.BlockTree(ctx, func(block blocks.Block) {
+		switch launchpad := block.(type) {
+		case blocks.LaunchPad:
+			for _, rocket := range launchpad.Rockets {
+				HeaderLinks = append(HeaderLinks, HeaderLink{
+					Href:    rocket.Href(),
+					Display: rocket.Display(),
+				})
+			}
 		}
-	}
+	})
 }
 
 // HeaderLink represents a header link. Header links are the links shown in the top gray bar.
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/bouncepaw/mycorrhiza
 go 1.17
 
 require (
-	github.com/bouncepaw/mycomarkup/v3 v3.4.1
+	github.com/bouncepaw/mycomarkup/v3 v3.5.0
 	github.com/go-ini/ini v1.63.2
 	github.com/gorilla/feeds v1.1.1
 	github.com/gorilla/mux v1.8.0
@@ -14,7 +14,6 @@ require (
 )
 
 require (
-	github.com/chekoopa/go-localize v0.4.0 // indirect
 	github.com/kr/pretty v0.2.1 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 	golang.org/x/sys v0.0.0-20211109184856-51b60fd695b3 // indirect
@@ -22,7 +21,7 @@ require (
 
 // Use this trick to test local Mycomarkup changes, replace the path with yours,
 // but do not commit the change to the path:
-//   replace github.com/bouncepaw/mycomarkup/v3 v3.4.1 => "/Users/bouncepaw/GolandProjects/mycomarkup"
+//   replace github.com/bouncepaw/mycomarkup/v3 v3.5.0 => "/Users/bouncepaw/GolandProjects/mycomarkup"
 
 // Use this utility every time Mycomarkup gets a major update:
 // https://github.com/marwan-at-work/mod
