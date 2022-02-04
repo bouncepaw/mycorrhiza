e98b10b56bbc5f3716966d52d4cd8665d6cc544f
Timur Ismagilov <bouncepaw2@ya.ru>
2022-02-04 16:14:53 +0500

    Introduce EmptyHypha

--- hyphae/interface.go
+++ hyphae/interface.go
@@ -1,6 +1,26 @@
 package hyphae
 
-import "sync"
+import (
+	"regexp"
+	"strings"
+	"sync"
+)
+
+// HyphaPattern is a pattern which all hyphae names must match.
+var HyphaPattern = regexp.MustCompile(`[^?!:#@><*|"'&%{}]+`)
+
+// IsValidName checks for invalid characters and path traversals.
+func IsValidName(hyphaName string) bool {
+	if !HyphaPattern.MatchString(hyphaName) {
+		return false
+	}
+	for _, segment := range strings.Split(hyphaName, "/") {
+		if segment == ".git" || segment == ".." {
+			return false
+		}
+	}
+	return true
+}
 
 type HyphaKind int
 
@@ -41,4 +61,44 @@ func RenameHyphaTo(h Hypher, newName string) {
 	byNames[h.CanonicalName()] = h.(*MediaHypha)
 	byNamesMutex.Unlock()
 	h.Unlock()
+}
+
+// insert inserts the hypha into the storage. A previous record is used if possible. Count incrementation is done if needed.
+func insert(h Hypher) (madeNewRecord bool) {
+	hp, recorded := byNames[h.CanonicalName()]
+	if recorded {
+		hp.(*MediaHypha).mergeIn(h)
+	} else {
+		storeHypha(h)
+		incrementCount()
+	}
+
+	return !recorded
+}
+
+// InsertIfNew checks whether hypha exists and returns `true` if it didn't and has been created.
+func InsertIfNew(h Hypher) (madeNewRecord bool) {
+	if h.DoesExist() {
+		return false
+	}
+	return insert(h)
+}
+
+func storeHypha(h Hypher) {
+	byNamesMutex.Lock()
+	byNames[h.CanonicalName()] = h
+	byNamesMutex.Unlock()
+
+	h.Lock()
+	h.(*MediaHypha).Exists = true
+	h.Unlock()
+}
+
+// ByName returns a hypha by name. It may have been recorded to the storage.
+func ByName(hyphaName string) (h Hypher) {
+	h, recorded := byNames[hyphaName]
+	if recorded {
+		return h
+	}
+	return NewEmptyHypha(hyphaName)
 }
