5ffa3f27dd8ba3d440d4cd0968f41e8a521ce3c1
Umar Getagazov <umar@handlerug.me>
2022-08-17 11:12:22 +0300

    Remove unnecessary comments

--- flag.go
+++ flag.go
@@ -41,7 +41,6 @@ func parseCliArgs() {
 	flag.Parse()
 
 	if versionFlag {
-		//fmt.Println("mycorrhiza:", version.TaggedRelease, "\tcommit:", version.CommitHash)
 		fmt.Println(version.FormatVersion())
 		os.Exit(0)
 	}
