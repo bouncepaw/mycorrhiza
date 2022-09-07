bouncepaw/mycorrhiza

commit 552aa12b8c6c865d3c9c0059f295cb8294d5b2a4

Set Git commiter name via environment

history/history.go
@@ -21,6 +21,8 @@ var gitpath string
 
 var renameMsgPattern = regexp.MustCompile(`^Rename ‘(.*)’ to ‘.*’`)
 
+var gitEnv = []string{"GIT_COMMITTER_NAME=wikimind", "GIT_COMMITTER_EMAIL=wikimind@mycorrhiza"}
+
 // Start finds git and initializes git credentials.
 func Start() {
 	path, err := exec.LookPath("git")
@@ -28,15 +30,6 @@ func Start() {
 		log.Fatal("Could not find the git executable. Check your $PATH.")
 	}
 	gitpath = path
-
-	_, err = silentGitsh("config", "user.name", "wikimind")
-	if err != nil {
-		log.Fatal(err)
-	}
-	_, err = silentGitsh("config", "user.email", "wikimind@mycorrhiza")
-	if err != nil {
-		log.Fatal(err)
-	}
 }
 
 // Revision represents a revision, duh. Hash is usually short. Username is extracted from email.
@@ -170,6 +163,7 @@ func gitsh(args ...string) (out bytes.Buffer, err error) {
 	fmt.Printf("$ %v\n", args)
 	cmd := exec.Command(gitpath, args...)
 	cmd.Dir = cfg.WikiDir
+	cmd.Env = gitEnv
 
 	b, err := cmd.CombinedOutput()
 	if err != nil {
@@ -182,6 +176,7 @@ func gitsh(args ...string) (out bytes.Buffer, err error) {
 func silentGitsh(args ...string) (out bytes.Buffer, err error) {
 	cmd := exec.Command(gitpath, args...)
 	cmd.Dir = cfg.WikiDir
+	cmd.Env = gitEnv
 
 	b, err := cmd.CombinedOutput()
 	return *bytes.NewBuffer(b), err

commit 7de805c00f82ab067ba5e16b56d7c9dae159ae19

Inherit environment in gitsh
Fixes the bug introduced in 552aa12b8c6c865d3c9c0059f295cb8294d5b2a4.

history/history.go
@@ -56,7 +56,7 @@ func gitsh(args ...string) (out bytes.Buffer, err error) {
 	fmt.Printf("$ %v\n", args)
 	cmd := exec.Command(gitpath, args...)
 	cmd.Dir = files.HyphaeDir()
-	cmd.Env = gitEnv
+	cmd.Env = append(cmd.Environ(), gitEnv...)
 
 	b, err := cmd.CombinedOutput()
 	if err != nil {